3ff8df557833bfb0d5447073a638779c42103fc7
Nick Vorobiow <vorobyev.nikitos@gmail.com>
2022-12-29 22:03:59 +0300

    выполнено

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,15 @@
 public class Solution {
-
+    public static int[] courseWorkTask(int[] arr) {
+        int[] arrBuf = new int[5];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i]++;
+            }
+            if (i >= 3 && i <= 7) {
+                arrBuf[index++] = arr[i];
+            }
+        }
+        return arrBuf;
+    }
 }
